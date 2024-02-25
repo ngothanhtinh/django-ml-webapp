43d4fbb0037265aaf4e0f17bf7780862077319d8
Ngo Thanh Tinh <thanhtinh.khaithaca53@gmail.com>
2024-02-25 21:26:07 +0700

    Move model to predictor

--- predictor/dashboard/forms.py
+++ predictor/dashboard/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Data
 
+
 class DataForm(forms.ModelForm):
     class Meta:
         model = Data
-        fields = ['name', 'age', 'height', 'sex']
+        fields = ["name", "age", "height", "sex"]
